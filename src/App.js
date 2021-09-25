0362350735e8573f630467b4268f5ec1be29279d
Vaibhav Patel <vppatel146@gmail.com>
2021-09-25 18:30:40 +0530

    done template intigration

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from "react";
-import logo from "./logo.svg";
-import { Counter } from "./features/counter/Counter";
-import "./App.css";
 import Header from "./Components/Admin/Header";
 import Menu from "./Components/Admin/Menu";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Header></Header>
-      <Menu></Menu>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <Header></Header>
+            <Menu></Menu>
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
